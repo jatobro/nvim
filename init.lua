6e10f879af08a57bb059b714e5214cbdff2f7cc7
Jakob T??ssebro <jakob.tossebro@gmail.com>
2026-04-01 10:26:05 +0200

    telescope with devicons, actions preview using telescope

--- init.lua
+++ init.lua
@@ -14,11 +14,48 @@ vim.schedule(function() vim.o.clipboard = "unnamedplus" end)
 vim.pack.add({
 	"https://github.com/vague-theme/vague.nvim",
 	"https://github.com/nvim-treesitter/nvim-treesitter",
+	"https://github.com/aznhe21/actions-preview.nvim",
 	"https://github.com/nvim-telescope/telescope.nvim",
+	"https://github.com/nvim-lua/plenary.nvim",
+	"https://github.com/nvim-tree/nvim-web-devicons",
 	"https://github.com/neovim/nvim-lspconfig",
 	"https://github.com/kdheepak/lazygit.nvim",
 	{ src = "https://github.com/Saghen/blink.cmp", version = vim.version.range("*") },
 	"https://github.com/L3MON4D3/LuaSnip"
+})
+
+require("telescope").setup({
+	defualts = {
+		preview = { treesitter = true },
+		color_devicons = true,
+		sorting_strategy = "ascending",
+		borderchars = {
+			"",
+			"",
+			"",
+			"",
+			"",
+			"",
+			"",
+			"",
+		},
+		path_displays = { "smart" },
+		layout_config = {
+			height = 100,
+			width = 400,
+			prompt_position = "top",
+			preview_cutoff = 40,
+		}
+	}
+})
+
+require("actions-preview").setup({
+	backend = { "telescope" },
+	extensions = { "env" },
+	telescope = vim.tbl_extend(
+		"force",
+		require("telescope.themes").get_dropdown(), {}
+	)
 })
 
 require("blink.cmp").setup({
@@ -27,18 +64,27 @@ require("blink.cmp").setup({
 	sources = {
 		default = { "lsp", "path", "snippets", "buffer" },
 	},
-	fuzzy = { implementation = "prefer_rust_with_warning" },
+	fuzzy = { implementation = "lua" },
 })
 
-vim.lsp.enable({ "lua_ls", "rust_analyzer" })
+vim.lsp.enable({ "lua_ls", "rust_analyzer", "ruff", "pyright", "tailwindcss", "ts_ls" })
 
 vim.g.mapleader = " "
 
 vim.keymap.set("n", "<leader>w", "<Cmd>update<CR>")
 vim.keymap.set("n", "<leader>q", "<Cmd>:quit<CR>")
-vim.keymap.set("n", "<leader>e", vim.cmd.Ex)
+
 vim.keymap.set("n", "<leader>lf", vim.lsp.buf.format, { desc = "Format current buffer" })
+
+vim.keymap.set("n", "<leader>e", vim.cmd.Ex, { desc = "Open file tree" })
 vim.keymap.set("n", "<leader>lg", vim.cmd.LazyGit, { desc = "Lazy Git" })
+
+local builtin = require("telescope.builtin")
+vim.keymap.set("n", "<leader>ff", builtin.find_files, { desc = "Telescope find files" })
+vim.keymap.set("n", "<leader>fg", builtin.live_grep, { desc = "Telescope live grep" })
+vim.keymap.set("n", "<leader>fb", builtin.buffers, { desc = "Telescope buffers" })
+
+vim.keymap.set("n", "<leader>sa", require("actions-preview").code_actions)
 
 vim.api.nvim_create_autocmd("TextYankPost", {
 	group = vim.api.nvim_create_augroup("highlight-yank", { clear = true }),
