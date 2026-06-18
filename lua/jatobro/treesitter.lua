efed8f3b6ba2c5e248375e13ba1929cbe7380e53
Jakob Tøssebro <jakob.tossebro@gmail.com>
2026-06-18 12:19:52 +0200

    ensure installed and auto install treesitter

--- lua/jatobro/treesitter.lua
+++ lua/jatobro/treesitter.lua
@@ -1,1 +1,17 @@
-require("nvim-treesitter").setup({})
+require("nvim-treesitter").setup({
+	ensure_installed = {
+		"lua",
+		"javascript",
+		"typescript",
+		"tsx",
+		"html",
+		"css",
+		"json",
+		"markdown",
+		"markdown_inline",
+		"bash",
+		"python",
+		"rust",
+	},
+	auto_install = true,
+})
